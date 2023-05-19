c91ec4a894ffca770ff736f3ee8ba9eef98c0082
Julian <julianser@hotmail.com.ar>
2023-05-19 17:59:48 -0300

    Se creo el componente TituloFormulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import TituloFormulario from './components/TituloFormulario'
 function App() {
 
   return (
     <>
-      
+     <Container className='container-fluid text-center bg-success'>
+      <TituloFormulario></TituloFormulario>
+     </Container>
+      <Container className='main'>
+
+      </Container>
+      <footer className='bg-dark text-light py-4 text-center'>
+        <p>&copy; Todos los Derechos Reservados</p>
+      </footer>
     </>
   )
 }
